2fa637773ed40272315a65e8a1fe25bb89eea418
RoxyRock <158159059+roxyrock@users.noreply.github.com>
2025-09-18 11:16:21 +0200

    implement Chunky Monkey algorithm with loop and slice

---
+++ README.md
@@ -0,0 +1,4 @@
+## Chunky Monkey Lab – JavaScript ##
+- Part of freeCodeCamp curriculum
+- This function named chunkArrayInGroups takes an array and a number, and splits the array into smaller arrays of the given length. 
+- Returns the result as a two-dimensional array.
---
+++ script.js
@@ -0,0 +1,15 @@
+function chunkArrayInGroups(array, size){
+  const result = [];
+  for(let i = 0; i < array.length; i+=size){
+    result.push(array.slice(i,  i+size));    
+  }
+  return result;
+}
+
+console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2));
